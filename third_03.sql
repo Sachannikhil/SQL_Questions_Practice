5f1b664dbcf7237854a5277f40acf4f4c2ee6e40
Nikhil Sachan <142492031+Sachannikhil@users.noreply.github.com>
2025-09-09 13:23:04 +0530

    Delete third_03.sql

--- third_03.sql
+++
@@ -1,4 +0,0 @@
--- QUESTION 3). Find the all movies that have the rating higher than the average rating of movies in the same genre --
-
-SELECT * FROM subquery.movies m1
-WHERE score > (SELECT AVG(score) FROM subquery.movies m2 WHERE m2.genre = m1.genre)
