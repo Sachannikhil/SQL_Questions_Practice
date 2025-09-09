7259ab80dcde8776bfbbbb09f4a3d20f8227b9d1
Nikhil Sachan <142492031+Sachannikhil@users.noreply.github.com>
2025-09-09 13:22:29 +0530

    Delete 02.sql

--- 02.sql
+++
@@ -1,6 +0,0 @@
--- QUESTION :- Find the Highest rated movie of each genre votes cut-of off 25000 --
-
-SELECT * FROM subquery.movies
-WHERE (genre,score) IN (SELECT genre,MAX(SCORE) FROM subquery.movies
-WHERE votes > 25000
-GROUP BY genre)
